72e4bade83c3f601c0394872ffab0f67981e17db
brahim elguabdi <96993446+elguabdi@users.noreply.github.com>
2022-06-10 18:34:01 +0100

    get data

--- app/src/main/java/com/example/lorem2/MainActivity.java
+++ app/src/main/java/com/example/lorem2/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
       EditText name ;
-      Button btn_add;
+      Button btn_add,btn_show;
+      ListView list_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +23,20 @@ public class MainActivity extends AppCompatActivity {
         MyDatabase db= new MyDatabase(this,"scole",null,1);
         name=findViewById(R.id.txt_name);
         btn_add=findViewById(R.id.btn_add);
+        btn_show=findViewById(R.id.btn_show);
+        list_view=findViewById(R.id.list_view_stude);
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                      db.add(name.getText().toString());
+            }
+        });
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> list_stude=db.get_table();
+                ArrayAdapter<String> adap=new ArrayAdapter<String>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,list_stude);
+                list_view.setAdapter(adap);
             }
         });
     }
--- app/src/main/java/com/example/lorem2/MyDatabase.java
+++ app/src/main/java/com/example/lorem2/MyDatabase.java
@@ -1,13 +1,19 @@
 package com.example.lorem2;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class MyDatabase extends SQLiteOpenHelper {
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class MyDatabase extends SQLiteOpenHelper
+{
 
     public MyDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -29,5 +35,19 @@ public class MyDatabase extends SQLiteOpenHelper {
         cv.put("name",n);
         db.insert("student",null,cv);
         db.close();
+    }
+    //methid get table
+    public   ArrayList<String> get_table()
+    {
+        ArrayList<String> list_student = new ArrayList<String>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cs = db.rawQuery("select * from student",null);
+         while (!cs.isAfterLast()){
+             String n = cs.getString(1);
+             list_student.add(n);
+             cs.moveToNext();
+         }
+         db.close();
+         return list_student;
     }
 }
